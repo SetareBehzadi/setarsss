4cebd04983fa81c6ca7e804f9da8b468dfb36e1f
bagher sefidi <@bagher>
2022-08-28 16:31:13 +0430

    complite index

--- src/_script/_component/index.js
+++ src/_script/_component/index.js
@@ -1,36 +1,38 @@
 /* ------------------------------------------------------------------------------------------ */
 /*                                          COMPONENT                                         */
 /* ------------------------------------------------------------------------------------------ */
-$('._responsive .menu-item-has-children > a').click(function(e) {
-	e.preventDefault();
-	var el = $(this);
-	// console.log(el);
-	var parentEl = el.parent();
-	parentEl.find('.menu-item-has-children').removeClass('open');
-	parentEl.siblings().removeClass('open');
-	parentEl.siblings().find('.menu-item-has-children').removeClass('open');
-	parentEl.toggleClass('open');
+$('._responsive .menu-item-has-children > a').click(function (e) {
+    e.preventDefault();
+    var el = $(this);
+    // console.log(el);
+    var parentEl = el.parent();
+    parentEl.find('.menu-item-has-children').removeClass('open');
+    parentEl.siblings().removeClass('open');
+    parentEl.siblings().find('.menu-item-has-children').removeClass('open');
+    parentEl.toggleClass('open');
 })
 
-$('.show_add_item').click(function(e) {
-	e.preventDefault();
-	$('.content_add_item').toggleClass('show');
+$('.show_add_item').click(function (e) {
+    e.preventDefault();
+    $('.content_add_item').toggleClass('show');
 })
 
-$('.has-child > a').click(function(e) {
-	e.preventDefault();
-	let parentEl = $(this).parent();
-	parentEl.toggleClass('active');
-	//parentEl.siblings().removeClass('active');
+$('.has-child > a').click(function (e) {
+    e.preventDefault();
+    let parentEl = $(this).parent();
+    parentEl.toggleClass('active');
+    //parentEl.siblings().removeClass('active');
 })
 
-// var swiper52 = new Swiper("#mySwiper", {
-// 	direction: "vertical",
-// 	// slidesPerView: "auto",
-// 	spaceBetween: 30,
-// 	slidesPerView: 3,
-// 	pagination: {
-// 		el: ".swiper-pagination",
-// 		clickable: true,
-// 	},
-// });
+var owl = $('.box-content-x1 .owl-carousel');
+owl.owlCarousel({
+    items: 5,
+    loop: true,
+    margin: 10,
+    rtl: true,
+    autoplay: true,
+    autoplayTimeout: 6000,
+    autoplayHoverPause: true,
+    dots: false,
+    nav:false
+});
